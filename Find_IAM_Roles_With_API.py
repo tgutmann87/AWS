5a42db39f2976926ed922738bd66d5b7b602244c
tgutmann87 <31452444+tgutmann87@users.noreply.github.com>
2024-01-22 10:17:21 -0500

    Add files via upload

--- Find_IAM_Roles_With_API.py
+++ Find_IAM_Roles_With_API.py
@@ -1,12 +1,13 @@
 import csv
 import sys
+import json
 import boto3
 
 iam_client = boto3.client('iam')
 
 with open('IAMPolicyDetails.csv', 'w', newline='') as file:
     file = csv.writer(file)
-    file.writerow(['Policy Name', 'Policy ARN', 'Default Version'])
+    file.writerow(['Policy Name', 'Policy ARN', 'Default Version', 'TypeError', 'Statement'])
      
     response = iam_client.list_policies()
     print("Getting IAM Policies...")
@@ -15,12 +16,23 @@ with open('IAMPolicyDetails.csv', 'w', newline='') as file:
         policyName = i['PolicyName']
         policyARN = i['Arn']
         policyDefaultVersion = i['DefaultVersionId']
-        print(policyName + " , " + policyARN + " , " + policyDefaultVersion)
+#        print(policyName + " , " + policyARN + " , " + policyDefaultVersion)
 
         policyResponse = iam_client.get_policy_version(PolicyArn=policyARN, VersionId=policyDefaultVersion)
-
-        if(str(policyResponse['PolicyVersion']['Document']).find("ssm:StartSession") >= 0):
-            file.writerow([policyName, policyARN, policyDefaultVersion])
+        
+        try:
+            for statement in policyResponse['PolicyVersion']['Document']['Statement']:
+                try:
+                    if statement['Action'].count('ssm:StartSession') > 0:
+                        file.writerow([policyName, policyARN, policyDefaultVersion, '', statement])
+                except KeyError:
+                    try:
+                        if statement['NotAction'].count('ssm:StartSession') > 0:
+                            file.writerow([policyName, policyARN, policyDefaultVersion, '', statement, ''])
+                    except:
+                        print("KeyError: No Action or NotAction Key")
+        except TypeError:
+            file.writerow([policyName, policyARN, policyDefaultVersion, 'X', policyResponse['PolicyVersion']['Document']])
                     
     while response['IsTruncated']:
         response = iam_client.list_policies(Marker=response['Marker'])
@@ -30,9 +42,20 @@ with open('IAMPolicyDetails.csv', 'w', newline='') as file:
             policyName = i['PolicyName']
             policyARN = i['Arn']
             policyDefaultVersion = i['DefaultVersionId']
-            print(policyName + " , " + policyARN + " , " + policyDefaultVersion)
+#            print(policyName + " , " + policyARN + " , " + policyDefaultVersion)
 
             policyResponse = iam_client.get_policy_version(PolicyArn=policyARN, VersionId=policyDefaultVersion)
 
-            if(str(policyResponse['PolicyVersion']['Document']).find("ssm:StartSession") >= 0):
-                file.writerow([policyName, policyARN, policyDefaultVersion])
+            try:
+                for statement in policyResponse['PolicyVersion']['Document']['Statement']:
+                    try:
+                        if statement['Action'].count('ssm:StartSession') > 0:
+                            file.writerow([policyName, policyARN, policyDefaultVersion, '', statement])
+                    except KeyError:
+                        try:
+                            if statement['NotAction'].count('ssm:StartSession') > 0:
+                                file.writerow([policyName, policyARN, policyDefaultVersion, '', statement])
+                        except:
+                            print("KeyError: No Action or NotAction Key")
+            except TypeError:
+                file.writerow([policyName, policyARN, policyDefaultVersion, 'X', policyResponse['PolicyVersion']['Document']])
